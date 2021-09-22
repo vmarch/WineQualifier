2f64fc247cdce7d842b7f6d19edfed312b5bc4ce
vmarch <marchenkoukr@gmail.com>
2021-09-22 19:41:36 +0200

    Initial Commit

---
+++ WineQualifier/WineQualifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineQualifierApp.swift
+//  WineQualifier
+//
+//  Created by Student on 22.09.21.
+//
+
+import SwiftUI
+
+@main
+struct WineQualifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
